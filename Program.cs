bcc86145ab6f642795af41f72fa23b5905a5a80b
brianmb18@gmail.com <85795924+Brianmb900@users.noreply.github.com>
2024-01-03 16:04:51 -0300

    Desafio Solucionado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado
+
+Iphone iphone = new Iphone("(11) 22334-5566", "iPhone 12", "0123456789abcde", 64);
+Console.WriteLine("iPhone:");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("--------------------------------");
+
+Nokia nokia = new Nokia("(77) 88990-1122", "Nokia C12", "9876543210edcba", 64);
+Console.WriteLine("Nokia:");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
